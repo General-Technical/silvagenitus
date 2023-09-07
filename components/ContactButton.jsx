e46e5dbed44114a1dd10753da2416bd004c4ab16
SavageDrip-GT <108780694+SavageDrip-GT@users.noreply.github.com>
2023-09-07 14:29:27 +0800

    Update ContactButton.jsx

--- components/ContactButton.jsx
+++ components/ContactButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ContactButton = () => {
   return (
-    <a href="mailto:tessa@frest.com?subject=Silvagentitus Enquiry">
+    <a href="mailto:tessa@fredst.com?subject=Silvagentitus Enquiry">
     <button type="submit" role="link" className="orderButton button">
       <h5 className="m-0 self-center button__text">Contact</h5>
     </button>
